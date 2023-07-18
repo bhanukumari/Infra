c53891344c282dd5c7f7085d73a8ae102fa6be60
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2023-07-18 21:57:04 +0530

    Update backend.tf

--- Terr-Code-Infra/Infra/infrastructure/env/dev/Databases/xampp/backend.tf
+++ Terr-Code-Infra/Infra/infrastructure/env/dev/Databases/xampp/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "sprint3-snaatak"
-    key     = "Ot-micro/env/dev/databases/mysql/terraform.tfstate"
+    bucket  = "bhanu-terraform"
+    key     = "Ot-micro/databases/mysql/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
